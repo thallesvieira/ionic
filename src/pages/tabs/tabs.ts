2bde32df69b766be93ce58b4b2664379a981dbcb
Thalles <thalles_jacobs@hotmail.com>
2018-06-20 21:13:30 -0300

    first commit

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { FeedPage } from '../feed/feed';
+import { IntroPage } from '../intro/intro';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -10,8 +12,8 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab4Root = FeedPage;
+  //tab5Root = IntroPage;
 
   constructor() {
 
